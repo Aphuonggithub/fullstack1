2174a8bd5abdbc3b9692fee680f636eb57d062ac
Phương Nguyễn <vietphuong461983@gmail.com>
2025-01-06 21:57:38 +0700

    CRUD-U

--- src/route/web.js
+++ src/route/web.js
@@ -1,17 +1,17 @@
 import express from "express";
 import homeController from "../controllers/homeController";
 
-
 let router = express.Router();
 
 let initWebRouters = (app) => {
   router.get("/", homeController.getHomePage);
-  router.get('/about', homeController.getAboutPage)
-  router.get('/crud', homeController.getCRUD)
+  router.get("/about", homeController.getAboutPage);
+  router.get("/crud", homeController.getCRUD);
 
-  router.post('/post-crud', homeController.postCrud)
-  router.get('/get-crud', homeController.displayGetCRUD)
-
+  router.post("/post-crud", homeController.postCrud);
+  router.get("/get-crud", homeController.displayGetCRUD);
+  router.get("/edit-crud", homeController.getEditCRUD);
+  router.post("/put-crud", homeController.putCRUD);
 
   return app.use("/", router);
 };
--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -1,31 +1,30 @@
 import bcrypt from "bcryptjs";
 import db from "../models/index";
 import { raw } from "body-parser";
+import { where } from "sequelize";
 
 const salt = bcrypt.genSaltSync(10);
 
 let createNewUser = async (data) => {
-    return new Promise(async(resolve, reject) =>{
-        try {
-            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-            await db.User.create({
-                email: data.email,
-                password: hashPasswordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                address:data.address,
-                phoneNumber: data.phoneNumber,
-                gender: data.gender ==="1" ? true : false,
-                
-                roleId: data.roleId,
-               
-            })
-            resolve(`Create a new user success`)
-        } catch (error) {
-            reject(error)
-        }
-    })
-  
+  return new Promise(async (resolve, reject) => {
+    try {
+      let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+      await db.User.create({
+        email: data.email,
+        password: hashPasswordFromBcrypt,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        address: data.address,
+        phoneNumber: data.phoneNumber,
+        gender: data.gender === "1" ? true : false,
+
+        roleId: data.roleId,
+      });
+      resolve(`Create a new user success`);
+    } catch (error) {
+      reject(error);
+    }
+  });
 };
 
 let hashUserPassword = (password) => {
@@ -39,21 +38,62 @@ let hashUserPassword = (password) => {
   });
 };
 
-let getAllUser =() =>{
-    return new Promise (async(resolve, reject) =>{
-try {
-    let users = await db.User.findAll({
-        raw : true
-    })
+let getAllUser = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let users = await db.User.findAll({
+        raw: true,
+      });
 
-    resolve(users)
-} catch (error) {
-    reject(error)
-}
-    })
-}
+      resolve(users);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
 
+let getUserInfoById = (userId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: userId },
+        row: true,
+      });
+      if (user) {
+        resolve(user);
+      } else {
+        resolve({});
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+let updateUserData = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: data.id },
+      });
+      if (user) {
+        user.firstName = data.firstName;
+        user.lastName = data.lastName;
+        user.address = data.address;
+        await user.save();
+        let allUsers = await db.User.findAll();
+        resolve(allUsers);
+      } else {
+        resolve();
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
 module.exports = {
-  createNewUser,
-  getAllUser,
+  createNewUser: createNewUser,
+  getAllUser: getAllUser,
+  getUserInfoById: getUserInfoById,
+  updateUserData: updateUserData,
 };
